86222d2e3651e2076db317480f6721080d816943
omarevolving@gmail.com <Kingoz08x!>
2021-10-04 04:33:36 -0400

    ok

--- thrones/src/components/Footer/footerNav.jsx
+++ thrones/src/components/Footer/footerNav.jsx
@@ -11,9 +11,9 @@ function FooterNav(props) {
         <div className = "footerContainer">
             <div className = "Footer" id = "Nav">
              <Link to = '/'className= "footerIcons" id="searchIcon"> <img src= {searchIcon} alt="Trophies"/> </Link><br></br>
-             <Link to = '/create' className= "footerIcons" id= "createIconFoot"><img src= { plusIcon } alt="Create NFT"/></Link><br></br>
+             <Link to = '/add-throne' className= "footerIcons" id= "createIconFoot"><img src= { plusIcon } alt="Create NFT"/></Link><br></br>
              <Link to = '/about' className= "footerIcons" id= "aboutIconFoot"><img src= "https://visualpharm.com/assets/454/About-595b40b65ba036ed117d42d0.svg" alt="About"/></Link><br></br>
-             <Link to = '/Account' className= "footerIcons" id= "loginIconFoot"><img src = { profile } alt= ""/></Link>     
+             <Link to = '/profile' className= "footerIcons" id= "loginIconFoot"><img src = { profile } alt= ""/></Link>     
         </div>
         </div>
         
